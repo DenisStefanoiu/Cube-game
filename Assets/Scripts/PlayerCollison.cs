3ab8b4bf84ade57ee4cca8b1ef5635c873710c90
Denis Ștefănoiu <denisstefanoiu@yahoo.com>
2023-11-06 17:12:45 +0200

    Add cube project

---
+++ Assets/Scripts/PlayerCollison.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerCollison : MonoBehaviour
+{
+    public PlayerMovement movement;
+    
+
+    void OnCollisonEnter (Collision collisonInfo)
+    {
+        if (collisonInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;//rb este prescurtarea de la Rigidbody
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); //Am adaugat o forta de miscare 
+
+
+        if (Input.GetKey("d"))
+        {
+
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0,ForceMode.VelocityChange);
+        }
+
+
+        if (Input.GetKey("a"))
+        {
+
+            rb.AddForce(-sidewaysForce * Time.deltaTime,0, 0,ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+
+        }
+    }
+}
